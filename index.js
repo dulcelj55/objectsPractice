b6c58a6a9abc6863e635f4bcb85eb42c58a10a69
Dulce Jaramillo <djaramillo@LABA.PSWAS.LOCAL>
2023-08-21 17:46:45 -0700

    three practice problems

---
+++ index.js
@@ -0,0 +1,64 @@
+// problem 1
+
+// carThree = {
+//     wheels: 5,
+//     owner: 'BOB',
+//     isBroken: true,
+//     color: "purple"
+
+// }
+// carThree.price = 800
+// carThree['year'] = 1999
+// carThree.isBroken = false
+
+// console.log(carThree.owner)
+// let clientIsInterested
+// if (carThree.color == "blue"){
+//     clientIsInterested = true
+// }else {
+//     clientIsInterested = false
+// }
+// console.log(clientIsInterested)
+// delete carThree.owner;
+// console.log(carThree)
+
+// problem 2
+// store= {
+//     profit: 500,
+//     manager: "Jackson",
+//     isOpenNow: true,
+//     isVeryExpensive :false, 
+
+// }
+// store.location = "Seattle"
+// store['storeType']= "brick"
+// store.isOpenNow = false
+// let canShopHere
+// if (store.isOpenNow == true && store.isVeryExpensive== true){
+//     canShopHere = true
+// }else{
+//     canShopHere = false
+// }
+// console.log(canShopHere)
+// delete store.profit 
+// console.log(store)
+// problem3 
+home={
+    rooms : 4,
+    bathrooms : 4,
+    isForSale : true,
+    isInGoodLocation: true
+}
+home.price = "54k"
+home['year']=1999
+home.isForSale= false,
+console.log(home.bathrooms)
+let buyHouse
+if (home.isForSale == true && home.isInGoodLocation  == true){
+    buyHouse = true
+}else{
+    buyHouse = false 
+}
+delete home.isForSale
+console.log(home)
+console.log(buyHouse)
